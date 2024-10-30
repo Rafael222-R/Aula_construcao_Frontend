6bfc177ac4a39ec5dcd59ce7105d8ba3b4eb6c4d
Rafael222-R <rafael_dias18@hotmail.com>
2024-10-30 05:51:38 -0300

    subir

---
+++ 08-projeto-iesb/src/app/page.js
@@ -0,0 +1,12 @@
+import Pagina from '@/components/Pagina'
+import React from 'react'
+
+
+export default function Homepage() {
+  return (
+    <Pagina titulo='Projeto IESB'>
+
+    </Pagina>
+ 
+  )
+}
