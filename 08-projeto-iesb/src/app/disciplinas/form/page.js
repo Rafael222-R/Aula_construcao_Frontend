231c24c7693c93fd73a92b06925f718fb0c7eda2
Rafael222-R <rafael_dias18@hotmail.com>
2024-11-02 00:28:14 -0300

    Atividade Disciplinas

---
+++ 08-projeto-iesb/src/app/disciplinas/form/page.js
@@ -0,0 +1,201 @@
+'use client'
+import Pagina from '@/components/Pagina'
+import { Formik } from 'formik'
+import { useRouter } from 'next/navigation'
+import { Button, Col, Form, Row } from 'react-bootstrap'
+import { FaArrowLeft, FaCheck, FaTrash } from 'react-icons/fa'
+import { v4 } from 'uuid'
+import * as Yup from 'yup'
+
+export default function disciplinasFormPage(props) {
+
+    const router = useRouter()
+
+    const cursos = JSON.parse(localStorage.getItem('cursos')) || []
+
+    const professores = JSON.parse(localStorage.getItem('professores')) || []
+
+    const disciplinas = JSON.parse(localStorage.getItem('disciplinas')) || []
+
+
+
+    const id = props.searchParams.id
+    console.log(props.searchParams.id)
+    const disciplinaEditada = disciplinas.find(item => item.id == id)
+    console.log(disciplinaEditada)
+
+    function salvar(dados) {
+
+        if (disciplinaEditada) {
+            Object.assign(disciplinaEditada, dados)
+
+            localStorage.setItem('disciplinas', JSON.stringify(disciplinas))
+        } else {
+
+            dados.id = v4()
+
+            disciplinas.push(dados)
+
+            localStorage.setItem('disciplinas', JSON.stringify(disciplinas))
+        }
+
+        alert("Disciplina criado com sucesso!")
+        router.push("/disciplinas")
+
+    }
+
+
+
+
+
+
+    const initialValues = {
+        nome: '',
+        discricao: '',
+        status: '',
+        curso: '',
+        professor: '',
+
+    }
+
+    const validationSchema = Yup.object().shape({
+        nome: Yup.string().required("Campo obrigatório"),
+        discricao: Yup.string().required("Campo obrigatório"),
+        status: Yup.string().required("Campo obrigatório"),
+        curso: Yup.string().required("Campo obrigatório"),
+        professor: Yup.string().required("Campo obrigatório"),
+    })
+
+
+
+
+    return (
+        <Pagina titulo={'Cadastro de Disciplinas'}>
+
+            <Formik
+
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={salvar}
+
+            >
+                {
+                    ({ values, errors, touched, handleChange, handleBlur, handleSubmit, handleReset }) => {
+
+                        return (
+                            <Form onSubmit={handleSubmit}>
+
+                                <Row className='mb-2'>
+                                    <Form.Group as={Col}>
+                                        <Form.Label>Nome:</Form.Label>
+                                        <Form.Control
+                                            name='nome'
+                                            type='text'
+                                            value={values.nome}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            isValid={touched.nome && !errors.nome}
+                                            isInvalid={touched.nome && !!errors.nome}
+                                        />
+                                        <Form.Control.Feedback type='invalid'>{errors.nome}</Form.Control.Feedback>
+                                    </Form.Group>
+
+                                    <Form.Group as={Col}>
+                                        <Form.Label>Discrição:</Form.Label>
+                                        <Form.Control
+                                            name='discricao'
+                                            type='text'
+                                            value={values.discricao}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            isValid={touched.discricao && !errors.discricao}
+                                            isInvalid={touched.discricao && !!errors.discricao}
+                                        />
+                                        <Form.Control.Feedback type='invalid'>{errors.discricao}</Form.Control.Feedback>
+                                    </Form.Group>
+                                </Row>
+
+                                <Row className='mb-2'>
+                                    <Form.Group as={Col}>
+                                        <Form.Label>Status:</Form.Label>
+                                        <Form.Select
+                                            name='status'
+                                            value={values.status}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            isValid={touched.status && !errors.status}
+                                            isInvalid={touched.status && !!errors.status}
+                                        >
+                                            <option value=''>Selecione</option>
+                                            <option value='Ativo'>Ativo</option>
+                                            <option value='Inativo'>Inativo</option>
+
+                                        </Form.Select>
+                                        <Form.Control.Feedback type='invalid'>{errors.status}</Form.Control.Feedback>
+                                    </Form.Group>
+
+                                    <Form.Group as={Col}>
+                                        <Form.Label>Curso:</Form.Label>
+                                        <Form.Select
+                                            name='curso'
+                                            value={values.curso}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            isValid={touched.curso && !errors.curso}
+                                            isInvalid={touched.curso && !!errors.curso}
+                                        >
+                                            <option value=''>Selecione</option>
+                                            {cursos.map(curso => <option value={curso.nome}>{curso.nome}</option>)}
+
+
+                                        </Form.Select>
+                                        <Form.Control.Feedback type='invalid'>{errors.curso}</Form.Control.Feedback>
+                                    </Form.Group>
+                                </Row>
+                                <Row className='mb-2'>
+                                    <Form.Group as={Col}>
+                                        <Form.Label>Professor:</Form.Label>
+                                        <Form.Select
+                                            name='professor'
+                                            value={values.professor}
+                                            onChange={handleChange}
+                                            onBlur={handleBlur}
+                                            isValid={touched.professor && !errors.professor}
+                                            isInvalid={touched.professor && !!errors.professor}
+                                        >
+                                            <option value=''>Selecione</option>
+                                            {professores.map(professor => <option value={professor.nome}>{professor.nome}</option>)}
+
+
+                                        </Form.Select>
+                                        <Form.Control.Feedback type='invalid'>{errors.professor}</Form.Control.Feedback>
+                                    </Form.Group>
+
+
+                                </Row>
+
+                                
+
+                                    <Form.Group >
+                                        <Button className='me-2' variant='secondary' href='/disciplinas'> <FaArrowLeft /> Voltar</Button>
+                                        <Button  className='text-end me-2' onClick={handleReset}> <FaTrash /> Limpar</Button>
+                                        <Button className='text-end me-2'type='submit' variant='success'> <FaCheck /> Enviar</Button>
+                                    </Form.Group>
+
+                                
+
+
+                            </Form>
+
+                        )
+                    }
+
+                }
+
+
+            </Formik>
+
+
+        </Pagina>
+    )
+}
---
+++ 08-projeto-iesb/src/app/disciplinas/page.js
@@ -0,0 +1,75 @@
+'use client'
+import Pagina from '@/components/Pagina'
+import { useEffect, useState } from 'react'
+import { Button, Table} from 'react-bootstrap'
+import { FaPen, FaPlusCircle, FaTrash } from 'react-icons/fa'
+
+
+export default function disciplinaPage() {
+
+    const [disciplinas, setDisciplinas] = useState([])
+
+    useEffect(() => {
+        const disciplinasLocalStorage = JSON.parse(localStorage.getItem("disciplinas")) || []
+   setDisciplinas(disciplinasLocalStorage)
+   console.log(disciplinasLocalStorage)
+   
+    }, [])
+
+    function excluir(disciplina) {
+
+        if(window.confirm(`Deseja realmente excluir a Disciplina ${disciplina.nome}?`)) {
+            const novaLista = disciplinas.filter(item => item.id !== disciplina.id)
+
+            localStorage.setItem('disciplinas', JSON.stringify(novaLista))
+            
+            setDisciplinas(novaLista)
+            alert('Disciplina excluida com Sucesso!')
+        }
+        
+    }
+
+
+  return (
+    <Pagina titulo={'Lista de Disciplinas'}>
+        <div className='text-end mb-2'>
+            <Button href='/disciplinas/form'><FaPlusCircle />Novo</Button>
+        </div>
+
+        <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Discrição</th>
+            <th>Status</th>
+            <th>Curso</th>
+            <th>Professor</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {disciplinas.map(disciplina => {
+            return (
+              <tr>
+                <td>{disciplina.nome}</td>
+                <td>{disciplina.discricao}</td>
+                <td>{disciplina.status}</td>
+                <td>{disciplina.curso}</td>
+                <td>{disciplina.professor}</td>
+                <td className='text-center'>
+                  {/* Botões das ações */}
+                  <Button className='me-2' href={`/disciplina/form?id=${disciplina.id}`}><FaPen /></Button>
+                  <Button variant='danger' onClick={() => excluir(disciplina)}><FaTrash /></Button>
+
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </Table>
+
+
+
+    </Pagina>
+  )
+}
